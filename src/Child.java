1cd29615dbe8e84fd1ea6eb2a94a4b498f8aa6ab
morzw <morzw@post.bgu.ac.il>
2020-12-27 13:14:28 +0200

    Main, Child, Device, eBracelet, ePark, eTicket

--- src/Child.java
+++ src/Child.java
@@ -5,5 +5,42 @@ public class Child {
     private String password;
     private eTicket eTicket;
 
+    public Child(String name, String ID, String password, int age, double timeLimit) {
+        this.name = name;
+        this.ID = ID;
+        this.password = password;
+        this.eTicket = new eTicket(ID, age, timeLimit);
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public eTicket geteTicket() {
+        return eTicket;
+    }
+
+    public void seteTicket(eTicket eTicket) {
+        this.eTicket = eTicket;
+    }
 }
